75c5d69859f29627aea4c1e736c0cc848a35fac2
waridrox <mohdwarid4@gmail.com>
2024-11-24 12:33:15 -0500

    Hashing passwords

--- server/api/user.post.js
+++ server/api/user.post.js
@@ -1,11 +1,31 @@
 // /api/user POST
 
+// Hashing passwords
+// - Prevents PW from being stored in plaintext
+// - mypassword123 jnjvsadcjncuwinuiwebjksab,/#@$fasDFVCASDR$@#
+
+// Salts
+// - salt = string of random characters
+// - Typically added to the beginning of a user's PW
+//    - mypassword123 becomes x#fSA#Amypassword123
+// - Used to prevent hackers from using precomputed hash tables to crack a PW
+// - Each user gets their own salt so even if two users have the same PW
+//   their password's look completely different
+
+import bcrypt from 'bcryptjs'
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
+
+  const salt = await bcrypt.genSalt(10)
+  const passwordHash = await bcrypt.hash(body.password, salt)
+
+  // Sends to database
   await prisma.user.create({
     data: {
       email: body.email,
-      password: body.password,
+      password: passwordHash,
+      salt: salt,
     },
   })
   return { data: 'success!' }
